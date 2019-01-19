104109204a6ba206816a70c4f8c87fbb7b508c01
yuryklim <yuryklim@outlook.com>
2019-01-19 14:12:28 +0200

    setting and patching values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,10 +28,25 @@ export class AppComponent implements OnInit {
     this.signupForm.statusChanges.subscribe(
       (status) => console.log(status)
     );
+
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Max',
+        'email': 'max@test.com'},
+      'gender': 'male',
+      'hobbies': []
+    });
+
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Yuri',
+        'email': 'max@test.com'}
+    });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
